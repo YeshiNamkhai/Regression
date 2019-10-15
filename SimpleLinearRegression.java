360f9f94252d79e79d3a3b3439bfd80bdedbe747
YeshiNamkhai <yeshi.namkhai@gmail.com>
2019-10-15 16:55:03 +0200

    verified

--- SimpleLinearRegression.java
+++ SimpleLinearRegression.java
@@ -5,6 +5,7 @@ import slr.ReadCSV;
 class SimpleLinearRegression {
  
     public static void main(String[] args) throws IOException{
+
         ReadCSV data = new ReadCSV();
         data.loadCSV("data.csv");
         data.printCSV();
