d05f5cb8929a2d821a25d90be4b06e38e32dd9d8
YeshiNamkhai <yeshi.namkhai@gmail.com>
2019-10-16 17:13:29 +0200

    verified

--- SimpleLinearRegression.java
+++ SimpleLinearRegression.java
@@ -1,4 +1,8 @@
+import java.io.File;
 import java.io.IOException;
+
+import com.panayotis.gnuplot.JavaPlot;
+import com.panayotis.gnuplot.dataset.FileDataSet;
 
 import slr.ReadCSV;
 
@@ -10,5 +14,15 @@ class SimpleLinearRegression {
         data.loadCSV("data.csv");
         data.printCSV();
         System.out.println(data.getCSV());
+        try {
+            JavaPlot p = new JavaPlot();
+            //p.set("autotitle columnhead", "''");
+            p.setTitle("Scatter Plot");
+            p.addPlot(new FileDataSet(new File("test.csv")));
+            System.out.println(p.getParameters());
+            p.plot();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 }
--- slr/Draw.java
+++ slr/Draw.java
@@ -1,5 +1,15 @@
 package slr;
 
+import com.panayotis.gnuplot.JavaPlot;
+
 public class Draw {
-    
+    public static void main(String[] args) {
+
+        JavaPlot p = new JavaPlot();
+
+        p.addPlot("sin(x)");
+
+        p.plot();
+
+    }
 }
