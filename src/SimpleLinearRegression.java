c93835accb5a482c754d02f43a65ed69e345543e
YeshiNamkhai <yeshi.namkhai@gmail.com>
2019-10-23 16:57:28 +0200

    ok

--- src/SimpleLinearRegression.java
+++ src/SimpleLinearRegression.java
@@ -1,23 +1,30 @@
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import slr.Compute;
 import slr.DataCSV;
 import slr.Draw;
 
 class SimpleLinearRegression {
  
     public static void main(String[] args) throws IOException{
-
-        DataCSV data = new DataCSV();
+       DataCSV data = new DataCSV();
         data.loadCSV("data.csv");
         data.printCSV();
-        System.out.println(data.getFields());
-        System.out.println(data.getValues());
-        System.out.println("X="+data.getIndexValues(0));
-        System.out.println("Y="+data.getIndexValues(1));
+        List<Double> x= new ArrayList<>(data.getIndexValues(0));
+        List<Double> xInv= new ArrayList<>(data.getIndexValuesInverted(0,4));
+        List<Double> y= new ArrayList<>(data.getIndexValues(1));
 
+        System.out.println();
+        System.out.format("%10s %10s %10s\n","Age","1/age","Height");
+        System.out.format("sum %6s %10s %10s\n",Compute.sumList(x),Compute.sumList(xInv),Compute.sumList(y));
+        System.out.format("avg %6s %10s %10s\n",Compute.avgList(x,4),Compute.avgList(xInv,4),Compute.avgList(y,4));
+        System.out.format("tss %17s %10s\n",Compute.tssList(xInv,4),Compute.tssList(y,4));
+        System.out.println();
+        System.out.format("rss %17s\n",Compute.rssList(xInv,y,4));
         
         Draw a = new Draw();
-
         String[] title = new String[] {"Dispersion Plot","Age","Height"};
         a.scatterPlot(data.getIndexValues(0),data.getIndexValues(1), title);
     }
--- src/slr/DataCSV.java
+++ src/slr/DataCSV.java
@@ -124,21 +124,4 @@ public class DataCSV {
         }
         return d;
     }
-
-    public static void main(String[] args) throws IOException {
-        DataCSV data = new DataCSV();
-        data.loadCSV("data.csv");
-        data.printCSV();
-        List<Double> x= new ArrayList<>(data.getIndexValues(0));
-        List<Double> xInv= new ArrayList<>(data.getIndexValuesInverted(0,4));
-        List<Double> y= new ArrayList<>(data.getIndexValues(1));
-
-        System.out.println();
-        System.out.format("%10s %10s %10s\n","Age","1/age","Height");
-        System.out.format("sum %6s %10s %10s\n",Compute.sumList(x),Compute.sumList(xInv),Compute.sumList(y));
-        System.out.format("avg %6s %10s %10s\n",Compute.avgList(x,4),Compute.avgList(xInv,4),Compute.avgList(y,4));
-        System.out.format("tss %17s %10s\n",Compute.tssList(xInv,4),Compute.tssList(y,4));
-        System.out.println();
-        System.out.format("rss %17s\n",Compute.rssList(xInv,y,4));
-    }
 }
